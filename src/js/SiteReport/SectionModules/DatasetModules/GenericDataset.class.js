b0f8e559557f8b8851bfc51fc73da21e415bb0cb
Johan von Boer <johan.von.boer@umu.se>
2020-03-26 15:12:07 +0100

    Cleaned up and documented version of GenericDataset module

--- src/js/SiteReport/SectionModules/DatasetModules/GenericDataset.class.js
+++ src/js/SiteReport/SectionModules/DatasetModules/GenericDataset.class.js
@@ -1,14 +1,14 @@
 import DatasetModule from "./DatasetModule.class";
-/*
+import shortid from "shortid";
+
+/**
 * Class: GenericDataset
 *
 * DatasetModule
 * 
 * This is a fall-back module that will capture all types of analyses/datasets which no other module claims and try to present some sort of basic rendering of some of the data.
 *
- */
-
-import shortid from "shortid";
+*/
 
 class GenericDataset extends DatasetModule {
 	constructor(analysis) {
@@ -23,21 +23,31 @@ class GenericDataset extends DatasetModule {
 		this.buildIsComplete = false;
 	}
 
+	/**
+	 * Function: offerAnalyses
+	 * 
+	 * Receives incoming offers of analyses/datasets to maybe claim / take responsibility for.
+	 * 
+	 * Parameters:
+	 * datasets - A list of objects describing datasets, is there any in this list you would like to take home with you? Open it up and let's see!
+	 * sectionsList - The finished "ContentItem" should be wrapped in a "Section" and that section should be appended to this 
+	 */
 	offerAnalyses(datasets, sectionsList) {
-		this.sectionsList = sectionsList;
-		for(let key = datasets.length - 1; key >= 0; key--) {
+		this.sectionsList = sectionsList; //Saving this for later
+
+		for(let key = datasets.length - 1; key >= 0; key--) { //Iterating through this backwards because that makes the splicing out of items less problematic
 			if(true) { //This module will always happily accept all datasets
-				console.log("Generic claiming ", datasets[key].datasetId);
-				let dataset = datasets.splice(key, 1)[0];
-				this.datasets.push(dataset);
+				console.log("Generic claiming ", datasets[key].datasetId); //Announce to the console that we are taking this dataset and running with it
+				let dataset = datasets.splice(key, 1)[0]; //Splice (remove) it from the list
+				this.datasets.push(dataset); //Add it to our own internal list of datasets we are going to care for
 
-				if(this.analysis.getMethodMetaById(dataset.methodId) === false) {
-					this.datasetFetchPromises.push(this.analysis.fetchMethodMetaData(dataset.methodId));
+				if(this.analysis.getMethodMetaById(dataset.methodId) === false) { //Try to get method meta data from cache first
+					this.datasetFetchPromises.push(this.analysis.fetchMethodMetaData(dataset.methodId)); //Otherwise we fetch it, using a function in the Analysis instance a step above us, since this is somehting all modules will want to do
 				}
-
 			}
 		}
 
+		//DatasetModules always returns a promise. If no datasets were claimed then that promise resolves right away. Otherwise it will resolve when the ContentItem has been built and appended to the main site report structure.
 		return new Promise((resolve, reject) => {
 			for(let key in this.datasets) {
 				let p = this.analysis.fetchAnalysis(this.datasets[key]);
@@ -55,22 +65,22 @@ class GenericDataset extends DatasetModule {
 		});
 	}
 	
-	destroy() {
-	}
-	
+	/**
+	 * Function: isBuildComplete
+	 * 
+	 * Don't think this function is used anymore.
+	 */
 	isBuildComplete() {
 		return this.buildIsComplete;
 	}
-	
 
 	/*
 	* Function: fetchDataset
 	*
-	* Gets the bloody samples. FIXME: PROBLEM IS: This is really only valid for MAL & Bugs data, what about dendro and ceramics and shit?
-	* I think we're gonna need different API/fetch-functions for different kinds of datasets, that's what I reckon mate.
+	* Will fetch some basic information about a dataset
 	*
 	* Parameters:
-	* datasetId
+	* dataset
 	 */
 	async fetchDataset(dataset) {
 		await new Promise((resolve, reject) => {
@@ -86,8 +96,14 @@ class GenericDataset extends DatasetModule {
 		});
 	}
 	
-	
-	
+	/**
+	 * Function: buildSection
+	 * 
+	 * Will build the "Section" containing the "ContentItem". A Section is a container in the site report, they can exist on different levels, this section exists on the lowest level (closest to the ContentItems) and it might contain several ContentItems. A ContentItem is a table or a graph - or some other form of rendering of data/content.
+	 * Note though that these are just descriptions/definitions of Sections and ContentItems created as a structure. This structure will then be passed on to functions and modules for rendering elsewhere in the site report system.
+	 * 
+	 * 
+	 */
 	buildSection(datasets) {
 		//Create sections
 		//We want to create as many sections as there are different types of methods in our datasets (usually just one though)
@@ -110,9 +126,14 @@ class GenericDataset extends DatasetModule {
 		});
 		
 		this.buildIsComplete = true;
-		this.hqs.hqsEventDispatch("siteAnalysisBuildComplete");
+		this.hqs.hqsEventDispatch("siteAnalysisBuildComplete"); //Don't think this is relevant anymore... I used events for determining if all DatasetModules had finished building before I switched to promises
 	}
 
+	/**
+	 * Function: appendDatasetToSection
+	 * 
+	 * Appends a dataset to a section by turning it into a ContentItem
+	 */
 	appendDatasetToSection(section, dataset) {
 		var analysis = dataset;
 		
@@ -153,6 +174,14 @@ class GenericDataset extends DatasetModule {
 			
 			section.contentItems[ciKey].data.rows.push(row);
 		}
+	}
+
+	/**
+	 * Function: destroy
+	 * 
+	 * Maybe this will be used for something one day...
+	 */
+	destroy() {
 	}
 	
 }
