1ee6c69eca1715b22bbcb3d320b5f290dbc7d78c
Johan von Boer <johan.von.boer@umu.se>
2020-05-07 14:21:22 +0200

    Work around for issue #121 that actually works!

--- src/js/FacetManager.class.js
+++ src/js/FacetManager.class.js
@@ -87,22 +87,28 @@ class FacetManager {
 		});
 
 		this.sqs.sqsEventListen("domainChanged", (evt, domainName) => {
-			this.buildFilterStructure(domainName);
-
 			//Disable/destroy all deployed facets which are not applicable in the current domain
 			let domain = this.sqs.domainManager.getDomain(domainName);
-			this.facets.forEach((facet) => {
+
+			//Workaround for issue https://github.com/humlab-sead/seadbrowserclient/issues/121
+			for(let i = this.facets.length - 1; i > -1; i--) {
+				let facet = this.facets[i];
 				facet.destroy();
-				/* Workaround for issue https://github.com/humlab-sead/seadbrowserclient/issues/121
+			}
+
+			/*
+			this.facets.forEach((facet) => {
 				if(!domain.filters.includes(facet.name)) {
 					console.log(facet.name+" doesn't exist in the current domain, deleting.");
 					facet.destroy();
 				}
-				*/
 			});
 
 			//Refresh all facets
 			this.chainQueueFacetDataFetch();
+			*/
+
+			this.buildFilterStructure(domainName);
 		});
 		
 	}
