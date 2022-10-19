79ea30152a4973cd7ddcde9e02bcca525a478366
Johan von Boer <johan.von.boer@umu.se>
2022-10-19 15:36:07 +0200

    can now handle the claiming of methods based on groups

--- src/js/SiteReport/SectionModules/DatasetModules/DatasetModule.class.js
+++ src/js/SiteReport/SectionModules/DatasetModules/DatasetModule.class.js
@@ -4,13 +4,38 @@ class DatasetModule {
     }
 
     claimDatasets(site) {
-		let methodDatasets = site.datasets.filter(dataset => {
-			return this.methodIds.includes(dataset.method_id);
+		let methodDatasets = [];
+		if(this.methodIds.length == 0 && typeof this.methodGroupIds != "undefined" && this.methodGroupIds.length > 0) {
+			methodDatasets = site.datasets.filter(dataset => {
+				return this.methodGroupIds.includes(dataset.method_group_id);
+			});
+		}
+		else {
+			methodDatasets = site.datasets.filter(dataset => {
+				return this.methodIds.includes(dataset.method_id);
+			});
+		}
+
+		let unclaimedDatasets = [];
+		site.datasets.forEach(dataset => {
+			let isClaimed = false;
+			methodDatasets.forEach(methodDataset => {
+				if(methodDataset.dataset_id == dataset.dataset_id) {
+					isClaimed = true;
+				}
+			});
+			if(!isClaimed) {
+				unclaimedDatasets.push(dataset);
+			}
 		});
 
+		site.datasets = unclaimedDatasets;
+		
+		/* this is much more elegant method for finding the unclaimed datasets, but it doesn't work with method groups...
 		site.datasets = site.datasets.filter(dataset => {
 			return !this.methodIds.includes(dataset.method_id);
 		});
+		*/
 
 		return methodDatasets;
 	}
