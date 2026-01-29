b7b6feca1f3c5edd92b2f978ff88a2d9aea2c00b
Johan von Boer <johan.von.boer@umu.se>
2026-01-29 16:30:50 +0100

    Updated scss version

--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,9 @@ const webpack = require('webpack');
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-require("ejs-compiled-loader");
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+require('ejs-compiled-loader');
+const BundleAnalyzerPlugin =
+  require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 const cesiumSource = path.resolve(__dirname, 'node_modules/cesium/Source');
@@ -25,101 +26,148 @@ module.exports = (env, config) => {
       filename: '[name].sead.bundle.js',
       publicPath: '/',
       sourcePrefix: '', // Required for Cesium
-      assetModuleFilename: '[name][ext][query]'
+      assetModuleFilename: '[name][ext][query]',
     },
+
+    // Enable Cesium to work with Webpack's AMD
     amd: {
-      // Enable Cesium to work with Webpack's AMD
-      toUrlUndefined: true
+      toUrlUndefined: true,
     },
+
     resolve: {
+      // Fix "process/browser" ESM fully-specified resolution + provide Node polyfills
+      alias: {
+        // Some ESM packages import `process/browser` without extension; force the fully-specified file.
+        'process/browser': require.resolve('process/browser.js'),
+      },
       fallback: {
-        "vm": require.resolve("vm-browserify"),
-        "crypto": require.resolve("crypto-browserify"),
-        "path": require.resolve("path-browserify"),
-        "stream": require.resolve("stream-browserify"),
-        "buffer": require.resolve("buffer/")
-      }
+        vm: require.resolve('vm-browserify'),
+        crypto: require.resolve('crypto-browserify'),
+        path: require.resolve('path-browserify'),
+        stream: require.resolve('stream-browserify'),
+        buffer: require.resolve('buffer/'),
+        // Add process polyfill (Webpack 5 no longer auto-polyfills)
+        process: require.resolve('process/browser'),
+      },
     },
+
     plugins: [
       new CleanWebpackPlugin(), // Ensures old files are deleted before build
+
       new webpack.DefinePlugin({
         'process.env.DEBUG': JSON.stringify(false),
         CESIUM_BASE_URL: JSON.stringify('/'),
       }),
+
       new HtmlWebpackPlugin({
         template: path.resolve(__dirname, './src/index.ejs'),
         filename: 'index.html',
         templateParameters: {
           baseUrl: JSON.stringify(seadConfig.serverRoot),
-        }
+        },
       }),
+
       new CopyWebpackPlugin({
         patterns: [
           { from: cesiumWorkers, to: 'Workers', globOptions: { nodir: true } },
-          { from: path.join(cesiumSource, 'Assets'), to: 'Assets', globOptions: { nodir: true } },
-          { from: path.join(cesiumSource, 'Widgets'), to: 'Widgets', globOptions: { nodir: true } },
-          { from: path.join(cesiumSource, 'ThirdParty'), to: 'ThirdParty', globOptions: { nodir: true } },
-          { from: path.resolve(__dirname, 'src/assets/data'), to: 'assets/data', globOptions: { nodir: true } },
+          {
+            from: path.join(cesiumSource, 'Assets'),
+            to: 'Assets',
+            globOptions: { nodir: true },
+          },
+          {
+            from: path.join(cesiumSource, 'Widgets'),
+            to: 'Widgets',
+            globOptions: { nodir: true },
+          },
+          {
+            from: path.join(cesiumSource, 'ThirdParty'),
+            to: 'ThirdParty',
+            globOptions: { nodir: true },
+          },
+          {
+            from: path.resolve(__dirname, 'src/assets/data'),
+            to: 'assets/data',
+            globOptions: { nodir: true },
+          },
         ],
       }),
+
       new webpack.ProvidePlugin({
         $: 'jquery',
         jQuery: 'jquery',
         Buffer: ['buffer', 'Buffer'], // Ensure Buffer is available
-        process: 'process/browser', // Ensure process is available
+        // Provide `process` global for deps expecting it
+        process: 'process/browser',
       }),
-      shouldAnalyze && new BundleAnalyzerPlugin({
-        analyzerMode: 'static',
-        reportFilename: 'bundle-report.html',
-        openAnalyzer: false,
-        generateStatsFile: true,
-        statsFilename: 'stats.json',
-      }),
-    ].filter(Boolean), // Removes `false` values to avoid issues
+
+      shouldAnalyze &&
+        new BundleAnalyzerPlugin({
+          analyzerMode: 'static',
+          reportFilename: 'bundle-report.html',
+          openAnalyzer: false,
+          generateStatsFile: true,
+          statsFilename: 'stats.json',
+        }),
+    ].filter(Boolean),
+
     module: {
       rules: [
+        // Compatibility shim for strict ESM "fully specified" imports
+        // This avoids failures when a package omits extensions in ESM imports.
+        {
+          test: /\.m?js$/,
+          resolve: {
+            fullySpecified: false,
+          },
+        },
+
         {
           test: /\.worker\.js$/,
-          use: { loader: 'worker-loader' }
+          use: { loader: 'worker-loader' },
         },
         {
           test: /\.ejs$/,
-          use: [{
-            loader: 'ejs-compiled-loader',
-            options: {
-              htmlmin: true,
-              htmlminOptions: {
-                removeComments: true
-              }
-            }
-          }]
+          use: [
+            {
+              loader: 'ejs-compiled-loader',
+              options: {
+                htmlmin: true,
+                htmlminOptions: {
+                  removeComments: true,
+                },
+              },
+            },
+          ],
         },
         {
           test: /\.(?:html)$/i,
-          use: ['html-loader']
+          use: ['html-loader'],
         },
         {
           test: /\.webmanifest$/,
-          use: [{
-            loader: 'file-loader',
-            options: {
-              name: 'site.webmanifest',
-              outputPath: '/'
-            }
-          }],
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                name: 'site.webmanifest',
+                outputPath: '/',
+              },
+            },
+          ],
         },
         {
           test: /\.js$/,
           exclude: /node_modules/,
           use: [
             {
-              loader: "babel-loader",
+              loader: 'babel-loader',
               options: {
-                presets: ["@babel/preset-env"]
-              }
+                presets: ['@babel/preset-env'],
+              },
             },
-            "source-map-loader"
-          ]
+            'source-map-loader',
+          ],
         },
         {
           test: /\.(?:ico|gif|png|jpg|jpeg|svg|webp|xml)$/i,
@@ -131,24 +179,25 @@ module.exports = (env, config) => {
         },
         {
           test: /\.css$/,
-          use: ['style-loader', 'css-loader']
+          use: ['style-loader', 'css-loader'],
         },
         {
           test: /\.scss$/,
           use: [
-            "style-loader",
+            'style-loader',
             {
-              loader: "css-loader",
+              loader: 'css-loader',
               options: {
                 importLoaders: 1,
-                modules: { mode: "icss" },
+                modules: { mode: 'icss' },
               },
             },
-            "sass-loader"
+            'sass-loader',
           ],
         },
       ],
     },
+
     devServer: {
       host: '0.0.0.0',
       port: 8080,
@@ -156,11 +205,11 @@ module.exports = (env, config) => {
       hot: true,
       liveReload: true,
       historyApiFallback: {
-        index: '/index.html'
+        index: '/index.html',
       },
       client: {
-        overlay: false
-      }
-    }
+        overlay: false,
+      },
+    },
   };
 };
